3aaeca6db658a9c1308d3b62a46fbdbe37f6ec0a
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-02-14 12:13:09 +0700

    ลงทะบียนเสร็จแล้ว ล็อคอินเสร้จแล้ว กำลังทำ RunOfRunner

--- src/views/LoginRunner.jsx
+++ src/views/LoginRunner.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import { Box, Typography, Avatar, TextField, Button } from '@mui/material';
 import run from './../assets/images/run.png'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom' 
+import { useState } from 'react';
 
 function LoginRunner() {
+  //สร้าง state (ค่าใน component ที่จะเอาไปใช้งานหรือค่าที่สามารถเปลี่ยนแปลงได้) 
+  const [runnerUsername, setRunnerUsername] = useState('');
+  const [runnerPassword, setRunnerPassword] = useState('');
+
+  //สร้างฟังก์ชันเพื่อจัดการการกดปุ่ม Login
+  const handleLoginClick = async (e) => {
+    e.preventDefault();    
+
+    //validate ui
+    if(runnerUsername === '' || runnerPassword === ''){      
+      alert('กรุณากรอกข้อมูลให้ครบ');
+      return;
+    }
+
+    //ส่งข้อมูลไปตรวจสอบ DB
+    try{
+      const response = await fetch(`http://localhost:3030/runner/${runnerUsername}/${runnerPassword}`, {
+        method: 'GET',
+      })
+
+      if(response.status === 200){
+        // alert('ถูกต้องจ้าาาาาา');
+        //ก่อนที่จะเปิดไป เราจะเอาข้อมูลของผู้ใช้เก็บใน localStorage
+        const responseData = await response.json();
+        localStorage.setItem('runner', JSON.stringify(responseData["data"])); 
+
+        window.location.href = '/run/runofrunner';
+      }else if(response.status === 404){
+        alert('ผู้ใช้ไม่ถูกต้องจ้าาาา');
+      }else{
+        alert('Login ไม่สําเร็จ ลองใหม่อีกครั้ง');        
+      }
+      
+    }catch(error){
+      alert(`พบปัญหาในการทำงาน ลองใหม่อีกครั้ง หรือติดต่อผู้ดูแล : ${error}`);
+    }
+  }
+
   return (
     <>
       <Box sx={{ width: '100%' }}>
@@ -16,12 +55,15 @@ function LoginRunner() {
             <Typography>
               ป้อนชื่อผู้ใช้
             </Typography>
-            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 2}} label='Username'/>
+            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 2}} label='Username'
+                       value={runnerUsername} onChange={(e) => setRunnerUsername(e.target.value)}/>
             <Typography>
               ป้อนรหัสผ่าน
             </Typography>
-            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 4}} type='password' label='Password'/>
-            <Button variant='contained' fullWidth sx={{pt: 2, pb: 2, backgroundColor: '#011474'}}>
+            <TextField variant='outlined' fullWidth sx={{mt: 2, mb: 4}} type='password' label='Password'
+                       value={runnerPassword} onChange={(e) => setRunnerPassword(e.target.value)}/>
+            <Button variant='contained' fullWidth sx={{pt: 2, pb: 2, backgroundColor: '#011474'}}
+                    onClick={handleLoginClick}>
               Login
             </Button>
             <Typography sx={{mt: 4, textAlign: 'center'}}>
--- src/views/RunOfRunner.jsx
+++ src/views/RunOfRunner.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import RunCircleIcon from '@mui/icons-material/RunCircle';
 
 function RunOfRunner() {
   return (
-    <div>RunOfRunner</div>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" sx={{backgroundColor: '#ff0000'}}>
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <RunCircleIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Run of Runner by Ninnin DTI-SAU
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
   )
 }
 
